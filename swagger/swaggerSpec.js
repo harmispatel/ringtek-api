4af1ba043587f2519ce76e66c1dddd87b32927ef
vijay2528 <jadavvijayr@gmail.com>
2025-02-13 16:14:26 +0530

    make some apis

--- swagger/swaggerSpec.js
+++ swagger/swaggerSpec.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "https://ringtek-api.vercel.app/api",
+        url: "ringtek-api-five.vercel.app/api",
         description: "Local server",
       },
     ],
